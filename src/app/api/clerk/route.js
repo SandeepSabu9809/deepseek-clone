be235130bc15925d75214fe849ccd7e9d4333b16
sandeepsabu <sandeepsabu321@gmail.com>
2025-08-30 20:13:00 +0530

    till message changes

--- src/app/api/clerk/route.js
+++ src/app/api/clerk/route.js
@@ -1,8 +1,8 @@
 import {Webhook} from "svix";
 import connectDB from "../../../../config/db";
-import { User } from "@clerk/nextjs/dist/types/server";
+import { User } from "../../../../models/User";
 import { headers } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
     const wh = new Webhook();
@@ -46,7 +46,7 @@ export async function POST(req) {
 
         }
         
-    return NextRequest.json({message: "Event Recieved"})
+    return NextResponse.json({message: "Event Recieved"})
 
 
 }
