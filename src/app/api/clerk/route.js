d3a29963007cc67b6f0edba4306600044d1c2634
sandeepsabu <sandeepsabu321@gmail.com>
2025-08-30 19:55:29 +0530

    message till

---
+++ src/app/api/clerk/route.js
@@ -0,0 +1,52 @@
+import {Webhook} from "svix";
+import connectDB from "../../../../config/db";
+import { User } from "@clerk/nextjs/dist/types/server";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function POST(req) {
+    const wh = new Webhook();
+    const headerPayload = await headers()
+    const svixHeaders = {
+        "svix-id": headerPayload.get("svix-id"),
+        "svix-signature": headerPayload.get("svix-signature"),
+    };
+
+    // Get the payload and verify it
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+    const {data,type} = wh.verify(body,svixHeaders)
+
+    //Prepare the user  data to be saved in the database
+
+    const UserData = {
+        _id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+    };
+
+    await connectDB()
+
+    switch (type) {
+        case 'user.created':
+            await User.create(UserData)
+            break;
+    
+        case 'user.updated':
+            await User.findByIdAndUpdate(data.id, UserData)
+            break;
+
+        case 'user.deleted':
+            await User.findByIdAndDelete(data.id)
+            break;    
+
+        default:
+            break;
+
+        }
+        
+    return NextRequest.json({message: "Event Recieved"})
+
+
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,21 @@
 import Image from "next/image";
 import { useState } from "react";
 import { assets } from "../../assets/assets";
+import Sidebar from "../../components/Sidebar";
+import PromptBox from "../../components/PromptBox";
+import Message from "../../components/Message";
 
 
 export default function Home() {
    
   const [expand,setExpand] = useState(false);
   const [messages,setMessages] = useState([]);
-  const [isloading,setIsloading] = useState(false);
+  const [isLoading,setIsLoading] = useState(false);
  
   return (
     <div>
       <div className="flex h-screen">
-        {/**sidebar*/}
+        <Sidebar expand={expand} setExpand={setExpand} />
         <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full" >
             <Image 
@@ -31,11 +34,11 @@ export default function Home() {
             </>
           ):(
             <div>
-
+              <Message role='user' content='what is next js' />
             </div>
           )}
 
-          {/* promptbox */}
+          <PromptBox isLoading={isLoading} setIsLoading={setIsLoading} />
           <p className="text-xs absolute bottom-1 text-gray-500">AI-generated, for reference only</p>
 
         </div>
